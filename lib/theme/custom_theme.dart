36cb1e9bb4eb74a5e5ff8ebf8667111ab3d69c62
Benjamin Fischer <bennyfischer22@googlemail.com>
2025-02-07 07:28:56 +0100

    feat: add theme and colors

--- lib/theme/custom_theme.dart
+++ lib/theme/custom_theme.dart
@@ -1,3 +1,4 @@
+import 'package:designhub/theme/designhub_colors.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getCustomTheme() => ThemeData(
@@ -9,18 +10,85 @@ ThemeData getCustomTheme() => ThemeData(
           gapPadding: 16,
           borderSide: BorderSide.none,
         ),
-        hintStyle:
-            TextStyle(color: Colors.grey), // Optional: Stil für Hint-Text
+        hintStyle: TextStyle(color: DesignhubColors.grey),
       ),
       iconButtonTheme: IconButtonThemeData(
         style: ButtonStyle(
-          backgroundColor: WidgetStateProperty.all(Colors.white),
+          backgroundColor: WidgetStateProperty.all(DesignhubColors.white),
           shape: WidgetStateProperty.all(
             CircleBorder(),
           ),
-          overlayColor: WidgetStateProperty.all(Colors.black12),
-          shadowColor: WidgetStateProperty.all(Colors.black26),
+          overlayColor: WidgetStateProperty.all(DesignhubColors.black12),
+          shadowColor: WidgetStateProperty.all(DesignhubColors.black26),
           elevation: WidgetStateProperty.all(4),
+        ),
+      ),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: ButtonStyle(
+          shape: WidgetStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          side: WidgetStateProperty.all(
+            BorderSide(
+              width: 2,
+              color: DesignhubColors.primary,
+            ),
+          ),
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: DesignhubColors.primary,
+          foregroundColor: DesignhubColors.white,
+          textStyle: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 20,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24),
+          ),
+          padding: EdgeInsets.symmetric(vertical: 14),
+        ),
+      ),
+      segmentedButtonTheme: SegmentedButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: WidgetStateProperty.resolveWith<Color?>(
+            (states) {
+              if (states.contains(WidgetState.selected)) {
+                return DesignhubColors.white;
+              }
+              return DesignhubColors.grey300;
+            },
+          ),
+          foregroundColor: WidgetStateProperty.resolveWith<Color?>(
+            (states) {
+              if (states.contains(WidgetState.selected)) {
+                return DesignhubColors.black;
+              }
+              return DesignhubColors.grey700;
+            },
+          ),
+          shape: WidgetStateProperty.resolveWith<OutlinedBorder>(
+            (Set<WidgetState> states) {
+              if (states.contains(WidgetState.selected)) {
+                return RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(48),
+                );
+              }
+              return RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              );
+            },
+          ),
+          side: WidgetStateProperty.all(BorderSide(
+            color: DesignhubColors.grey300,
+            width: 4,
+          )),
+          padding: WidgetStateProperty.all(
+              EdgeInsets.symmetric(vertical: 10, horizontal: 16)),
+          animationDuration: Duration(milliseconds: 1),
         ),
       ),
     );
---
+++ lib/theme/designhub_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+abstract class DesignhubColors {
+  static final Color primary = Colors.deepOrange;
+  static final Color primary100 = Colors.deepOrange.shade100;
+
+  static final Color white = Colors.white;
+
+  static final Color red = Colors.red;
+
+  static final Color grey = Colors.grey;
+  static final Color grey100 = Colors.grey.shade100;
+  static final Color grey400 = Colors.grey.shade400;
+  static final Color grey300 = Colors.grey.shade300;
+  static final Color grey700 = Colors.grey.shade700;
+
+  static final Color black = Colors.black;
+  static final Color black12 = Colors.black12;
+  static final Color black26 = Colors.black26;
+  static final Color transparent = Colors.transparent;
+}
