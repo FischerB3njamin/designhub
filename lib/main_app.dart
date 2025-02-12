e8e8ed08beeb1e58d56bb8433e498e4494002fc8
Benjamin Fischer <bennyfischer22@googlemail.com>
2025-02-12 07:22:31 +0100

    ref: push mainapp in own file

---
+++ lib/main_app.dart
@@ -0,0 +1,16 @@
+import 'package:designhub/features/auth/view/login_page.dart';
+import 'package:designhub/theme/custom_theme.dart';
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: getCustomTheme(),
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+    );
+  }
+}
