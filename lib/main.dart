94246e27729f5dcd71481fbe38911487559abb16
Benjamin Fischer <bennyfischer22@googlemail.com>
2025-03-25 12:10:48 +0100

    feat: add firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:designhub/firebase_options.dart';
 import 'package:designhub/main_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
